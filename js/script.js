4480fb7f747bb601b36aefe5808e5e8f03b2ebb7
Federico Cuello <phfedericocuello@outlook.com>
2021-07-31 04:16:28 -0300

    arreglo tablet

--- js/script.js
+++ js/script.js
@@ -7,13 +7,14 @@ let aBotonServicios = document.getElementById("aBotonServicios");
 
 const cambiar = () => {
     if (masServicios == true) {
-    aBotonServicios.innerHTML = "Ver menos servicios";
-    masServicios = false;
-}else {
-    aBotonServicios.innerHTML = "Ver más servicios";
-    masServicios = true;
-
-}
+        aBotonServicios.innerHTML = "Ver menos servicios";
+        masServicios = false;
+        $('.acercaDe').css('margin-top', '110%');
+    } else {
+        aBotonServicios.innerHTML = "Ver más servicios";
+        masServicios = true;
+        $('.acercaDe').css('margin-top', '10%');
+    }
 }
 
 aBotonServicios.addEventListener("click", cambiar);
